71ff1b970379cab4da74532db1570a6d76eec75d
hempers <mabb2022-3195-82184@bicol-u.edu.ph>
2024-08-26 13:50:20 +0800

    added blade.php files

---
+++ Lab1/resources/views/content.blade.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Content</title>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
+  <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+
+  <header>
+    <div class="logo">
+        <h1 class="text">Laravel</h1>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="home.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="content.html">Content</a></li>
+        </ul>
+    </nav>
+</header>
+<section class="articles">
+  <article>
+    <div class="article-wrapper">
+      <figure>
+        <img src="assets/img/content/coverr.svg" alt="" />
+      </figure>
+      <div class="article-body">
+        <h2>Project 1</h2>
+      </div>
+    </div>
+  </article>
+  <article>
+
+    <div class="article-wrapper">
+      <figure>
+        <img src="assets/img/content/cover.svg" alt="" />
+      </figure>
+      <div class="article-body">
+        <h2>Project 2</h2>
+      </div>
+    </div>
+  </article>
+  <article>
+
+    <div class="article-wrapper">
+      <figure>
+        <img src="assets/img/content/coverrr.svg" alt="" />
+      </figure>
+      <div class="article-body">
+        <h2>Project 3</h2>
+      </div>
+    </div>
+  </article>
+</section>  
+</body>
+</html>
---
+++ Lab1/resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web Development</title>
+    <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Bebas+Neue&amp;family=Figtree:wght@300;600&amp;display=swap'>
+    <link rel="stylesheet" href="styles/styles_h.css">
+</head>
+<body>
+
+    <header>
+        <div class="logo">
+            <h1 class="text">Laravel</h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="content.html">Content</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div class="container">
+        <img src="assets/img/home/logo.svg" alt="Laravel Logo" class="logo">
+        <h1>Web Development</h1>
+        <h2>First Laravel Project</h2>
+        <a href="content.html">
+        <button>Explore More</button>
+        </a>
+        
+    </div>
+
+</body>
+</html>
